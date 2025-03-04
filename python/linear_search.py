a316a73b5d62f0aeb0dfa8226e34fb850490f611
robertuptc <robertuptc@gmail.com>
2025-03-04 13:33:43 -0500

    python problem finished

--- python/linear_search.py
+++ python/linear_search.py
@@ -1,6 +1,10 @@
-array_to_search_through = []
-for number in range(1, 1001):
-    array_to_search_through.append(number)
-
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    answer = []
+
+    for i, element in enumerate(array_to_search_through):
+        # print(value_to_find, element, value_to_find == element)
+        if value_to_find == element:
+            answer.append(i)
+    
+    return answer if len(answer) else None
+
